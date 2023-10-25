8d3f890f8906055da5a0cebfc3987d06c0cf6e06
arthuremment <artshiv13@gmail.com>
2023-10-25 20:47:17 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Count from './components/Count';
+import './App.css';
+
+function App() {
+  const [count, setCount] = React.useState(0)
+
+  function add(){
+    setCount(prevCount => prevCount + 1)
+  }
+  function subtract(){
+    setCount(prevCount => prevCount - 1)
+  }
+
+  return (
+    <div className='counter'>
+      <button className='counter--minus' onClick={subtract}>-</button>
+      <Count number={count} />
+      <button className='counter--plus' onClick={add}>+</button>
+    </div>
+  );
+}
+
+export default App;
